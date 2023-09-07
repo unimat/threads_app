c51d366cf37d3f3652b144f2d32510215975d044
unimat <unimat@unimat.hu>
2023-09-07 15:29:34 +0200

    Ignore Build Errors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
